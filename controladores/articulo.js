e520abec2eed35769feb86d11a74348e76b6b0e3
eldeividrig <davidgabrielb@hotmail.com>
2023-01-11 13:10:46 -0300

    segundo push

--- controladores/articulo.js
+++ controladores/articulo.js
@@ -1,4 +1,4 @@
-const validator = require("validator");
+const { validarArticulo } = require("../helpers/validar");
 const Articulo = require("../modelos/Articulo");
 
 const prueba = (req, res) => {
@@ -13,15 +13,7 @@ const crear = (req, res) => {
 
   //Validar Datos
   try {
-    let validar_titulo =
-      !validator.isEmpty(parametros.titulo) &&
-      validator.isLength(parametros.titulo, { min: 5, max: undefined });
-
-    let validar_contenido = !validator.isEmpty(parametros.contenido);
-
-    if (!validar_titulo || !validar_contenido) {
-      throw new Error("No se ha validado la información!!");
-    }
+    validarArticulo(parametros);
   } catch (error) {
     return res.status(400).json({
       status: "error",
@@ -48,12 +40,123 @@ const crear = (req, res) => {
     return res.status(200).json({
       status: "success",
       articulo: articuloGuardado,
-      mensaje: "Articulo creado con éxito!!"
+      mensaje: "Articulo creado con éxito!!",
     });
   });
 };
 
+const listar = (req, res) => {
+  let consulta = Articulo.find({});
+  if (req.params.ultimos) {
+    consulta.limit(req.params.ultimos);
+  }
+
+  consulta.sort({ fecha: -1 }).exec((error, articulos) => {
+    if (error || !articulos) {
+      return res.status(404).json({
+        status: "error",
+        mensaje: "No se han encontrado articulos!",
+      });
+    }
+
+    return res.status(200).send({
+      status: "success",
+      contador: articulos.length,
+      articulos,
+    });
+  });
+};
+
+const uno = (req, res) => {
+  //Recoger un id por la url
+  let id = req.params.id;
+  //Buscar el articulo
+  Articulo.findById(id, (error, articulo) => {
+    //Si no existe devolver error
+    if (error || !articulo) {
+      return res.status(404).json({
+        status: "error",
+        mensaje: "No se ha encontrado el articulo",
+      });
+    }
+    //Devolver resultado
+    return res.status(200).send({
+      status: "success",
+      articulo,
+    });
+  });
+};
+
+const borrar = (req, res) => {
+  let articuloId = req.params.id;
+
+  Articulo.findByIdAndDelete({ _id: articuloId }, (error, articuloBorrado) => {
+    if (error || !articuloBorrado) {
+      return res.status(500).json({
+        status: "error",
+        mensaje: "Error al borrar el articulo",
+      });
+    }
+    return res.status(200).send({
+      status: "success",
+      articulo: articuloBorrado,
+      mensaje: "Metodo de borrar",
+    });
+  });
+};
+
+const editar = (req, res) => {
+  //Recoger articulo a editar
+  let articuloId = req.params.id;
+
+  //Recoger datos del body
+  let parametros = req.body;
+
+  //Validar datos
+  try {
+    validarArticulo(parametros);
+  } catch (error) {
+    return res.status(400).json({
+      status: "error",
+      mensaje: "Faltan datos por enviar",
+    });
+  }
+
+  //Buscar y actualizar articulo
+  Articulo.findOneAndUpdate(
+    { _id: articuloId },
+    parametros,
+    { new: true },
+    (error, articuloActualizado) => {
+      if (error || !articuloActualizado) {
+        return res.status(500).json({
+          status: "error",
+          mensaje: "Error al actualizar el articulo",
+        });
+      }
+      //Devolver respuesta
+      return res.status(200).send({
+        status: "success",
+        articulo: articuloActualizado,
+        mensaje: "Articulo actualizado",
+      });
+    }
+  );
+};
+
+const subir = (req, res) => {
+  return res.status(200).send({
+    status: "success",
+  });
+}
+
+
 module.exports = {
   prueba,
   crear,
+  listar,
+  uno,
+  borrar,
+  editar,
+  subir,
 };
--- rutas/articulo.js
+++ rutas/articulo.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+
+
+const almacenamiento = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './image/articulos/');
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, "articulo" + Date.now() + file.originalname);
+    }
+})
 
 const ArticuloControlador = require("../controladores/articulo");
 
 //Rutas de pruebas
 router.get("/ruta-de-prueba", ArticuloControlador.prueba);
 router.post("/crear", ArticuloControlador.crear);
+router.get("/articulos/:ultimos?", ArticuloControlador.listar);
+router.get("/articulo/:id", ArticuloControlador.uno);
+router.delete("/articulo/:id", ArticuloControlador.borrar);
+router.put("/articulo/:id", ArticuloControlador.editar);
+router.post("/subir-imagen/:id", ArticuloControlador.subir);
 
 
 module.exports = router;
